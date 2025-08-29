16c2f6eabae679ae2eb81c59411806a17c7ea91c
Esther <devarsesther@gmail.com>
2025-08-29 08:22:18 +0200

    Add file modelefonction.php

---
+++ modelefonction.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fonction <code></code></title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <h1>Fonction <code></code></h1>
+
+    <h2>Description</h2>
+    <p>
+        <!-- Décris ici la fonction -->
+    </p>
+
+    <h2>Exemple</h2>
+
+    <h3>Code</h3>
+    <pre>
+&lt;?php
+// Place ici l'exemple de code PHP pour la fonction
+?&gt;
+    </pre>
+
+    <h3>Résultat</h3>
+    <p>
+        <?php
+        // Place ici le code PHP exécuté pour afficher le résultat
+        ?>
+    </p>
+</body>
+
+</html>
